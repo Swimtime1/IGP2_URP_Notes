e7efbcd71689c06fa0451bd8a0e5c7fcf774aed7
Swimtime1 <aiden.crossfield@gmail.com>
2024-10-11 11:43:46 -0400

    Procedural Walking
    The procedural walking mostly works now! I just need to make it vary with Player speed

--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -15,6 +15,10 @@ public class Walker : MonoBehaviour
     // Vector3 Variables
     private Vector3 leftOffset, rightOffset;
 
+    // Float Variables
+    private float leftLegLast = 0;
+    private float rightLegLast = 0;
+
     #endregion Variables
     
     // Start is called before the first frame update
@@ -27,11 +31,28 @@ public class Walker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float leftLegForwardMovement = horizontalCurve.Evaluate(Time.time);
+        float rightLegForwardMovement = horizontalCurve.Evaluate(Time.time - 1);
+        
         leftFootTarget.localPosition = leftOffset + 
-            (this.transform.InverseTransformVector(leftFootTarget.forward) * horizontalCurve.Evaluate(Time.time)) + 
+            (this.transform.InverseTransformVector(leftFootTarget.forward) * leftLegForwardMovement) + 
             (this.transform.InverseTransformVector(leftFootTarget.up) * VerticalCurve.Evaluate(Time.time + 0.5f));
         rightFootTarget.localPosition = rightOffset + 
-            (this.transform.InverseTransformVector(rightFootTarget.forward) * horizontalCurve.Evaluate(Time.time - 1)) + 
+            (this.transform.InverseTransformVector(rightFootTarget.forward) * rightLegForwardMovement) + 
             (this.transform.InverseTransformVector(rightFootTarget.up) * VerticalCurve.Evaluate(Time.time - 0.5f));
+        
+        float leftLegDirection = leftLegForwardMovement - leftLegLast;
+        float rightLegDirection = rightLegForwardMovement - rightLegLast;
+        
+        RaycastHit hit;
+        if(leftLegDirection < 0 &&
+            Physics.Raycast(leftFootTarget.position + leftFootTarget.up, -leftFootTarget.up, out hit, Mathf.Infinity))
+        { leftFootTarget.position = hit.point; }
+        if(rightLegDirection < 0 &&
+            Physics.Raycast(rightFootTarget.position + rightFootTarget.up, -rightFootTarget.up, out hit, Mathf.Infinity))
+        { rightFootTarget.position = hit.point; }
+
+        leftLegLast = leftLegForwardMovement;
+        rightLegLast = rightLegForwardMovement;
     }
 }
