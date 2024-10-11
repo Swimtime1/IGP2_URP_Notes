12145450b6fda47928a6ce7c6550f893be509cda
Swimtime1 <aiden.crossfield@gmail.com>
2024-10-11 12:22:20 -0400

    Procedural Walking
    The speed of the procedural walking animation now lines up with the animation affecting the top half of the model

--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -31,19 +31,27 @@ public class Walker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float leftLegForwardMovement = horizontalCurve.Evaluate(Time.time);
-        float rightLegForwardMovement = horizontalCurve.Evaluate(Time.time - 1);
+        float fEnd = horizontalCurve[1].time;
+        float uEnd = VerticalCurve[1].time;
         
+        float leftLegForwardMovement = horizontalCurve.Evaluate(Time.time);
+        float rightLegForwardMovement = horizontalCurve.Evaluate(Time.time - fEnd);
+        float leftLegUpwardMovement = VerticalCurve.Evaluate(Time.time + uEnd);
+        float rightLegUpwardMovement = VerticalCurve.Evaluate(Time.time - uEnd);
+        
+        // Moves the local position of the feet
         leftFootTarget.localPosition = leftOffset + 
             (this.transform.InverseTransformVector(leftFootTarget.forward) * leftLegForwardMovement) + 
-            (this.transform.InverseTransformVector(leftFootTarget.up) * VerticalCurve.Evaluate(Time.time + 0.5f));
+            (this.transform.InverseTransformVector(leftFootTarget.up) * leftLegUpwardMovement);
         rightFootTarget.localPosition = rightOffset + 
             (this.transform.InverseTransformVector(rightFootTarget.forward) * rightLegForwardMovement) + 
-            (this.transform.InverseTransformVector(rightFootTarget.up) * VerticalCurve.Evaluate(Time.time - 0.5f));
+            (this.transform.InverseTransformVector(rightFootTarget.up) * rightLegUpwardMovement);
         
+        // Determines if each leg is moving forward or backward
         float leftLegDirection = leftLegForwardMovement - leftLegLast;
         float rightLegDirection = rightLegForwardMovement - rightLegLast;
         
+        // Makes sure that both legs are grounded while moving backward
         RaycastHit hit;
         if(leftLegDirection < 0 &&
             Physics.Raycast(leftFootTarget.position + leftFootTarget.up, -leftFootTarget.up, out hit, Mathf.Infinity))
@@ -52,6 +60,7 @@ public class Walker : MonoBehaviour
             Physics.Raycast(rightFootTarget.position + rightFootTarget.up, -rightFootTarget.up, out hit, Mathf.Infinity))
         { rightFootTarget.position = hit.point; }
 
+        // Updates the last known direction of each leg
         leftLegLast = leftLegForwardMovement;
         rightLegLast = rightLegForwardMovement;
     }
