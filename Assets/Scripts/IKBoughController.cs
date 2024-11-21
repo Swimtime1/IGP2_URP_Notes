25f6fe6f226bd9777bd437971a9340f990a73a7c
Swimtime1 <124410615+Swimtime1@users.noreply.github.com>
2024-11-21 14:43:02 -0500

    Branches now follow apples

--- Assets/Scripts/IKBoughController.cs
+++ Assets/Scripts/IKBoughController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class IKBoughController : MonoBehaviour
 {
@@ -10,18 +11,27 @@ public class IKBoughController : MonoBehaviour
     [SerializeField] private Vector3 initPos;
     [SerializeField] private Vector3 awayPos;
 
+    [Header("Dimensions")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private float playerRadius = 5f;
+    [SerializeField] private float S;
+
     #endregion Variables
     
     // Start is called before the first frame update
     void Start()
     {
         initPos = transform.position;
-        awayPos = new Vector3(initPos.x, initPos.y + 5, initPos.z);
+        awayPos = new Vector3(initPos.x, initPos.y + playerRadius, initPos.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        S = Vector3.Distance(initPos, player.transform.position);
+        S /= playerRadius;
+        S = 1 - S;
+        S = Mathf.Clamp01(S);
+        transform.position = Vector3.Lerp(initPos, awayPos, S);
     }
 }
